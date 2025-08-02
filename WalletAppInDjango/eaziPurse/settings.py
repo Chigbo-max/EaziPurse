7df6170796f6bf5bd1c85c589cf851d6fc5d7706
Chigbo Ezeokeke <ezeokeke.chigbo@gmail.com>
2025-08-02 22:43:20 +0100

    updated the CORS settings

--- WalletAppInDjango/eaziPurse/settings.py
+++ WalletAppInDjango/eaziPurse/settings.py
@@ -218,7 +218,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",  
     "http://127.0.0.1:3000",
-    "https://eazipurse-ng.onrender.com",  # Add your production frontend domain
+    "https://eazipurse-ng.onrender.com", 
 ]
 
 CORS_ALLOW_CREDENTIALS = True
