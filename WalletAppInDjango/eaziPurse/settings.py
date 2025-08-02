741a8da547a71ab9005632297c2ab824427500ae
Chigbo Ezeokeke <ezeokeke.chigbo@gmail.com>
2025-08-02 22:53:35 +0100

    updated the allowed hosts

--- WalletAppInDjango/eaziPurse/settings.py
+++ WalletAppInDjango/eaziPurse/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,*.onrender.com').split(',')
 
 
 # Application definition
