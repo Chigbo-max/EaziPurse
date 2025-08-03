efe4f50b9c0333604ed1676cd203ac123f0ccca4
Chigbo Ezeokeke <ezeokeke.chigbo@gmail.com>
2025-08-03 21:44:18 +0100

    Fixed account number generation to create unique 10-digit numbers

--- WalletAppInDjango/wallet/signals.py
+++ WalletAppInDjango/wallet/signals.py
@@ -4,10 +4,22 @@ from django.conf import settings
 from django.dispatch import receiver
 
 
+import random
+import string
+
 @receiver(post_save,sender=settings.AUTH_USER_MODEL)
 def create_wallet(sender, instance, created, **kwargs):
     if created:
+        # Generate a unique 10-digit account number
+        def generate_account_number():
+            while True:
+                # Generate a 10-digit number starting with 1
+                account_number = '1' + ''.join(random.choices(string.digits, k=9))
+                # Check if it's unique
+                if not Wallet.objects.filter(account_number=account_number).exists():
+                    return account_number
+        
         Wallet.objects.create(
             user=instance,
-            account_number=instance.phone[1:]
+            account_number=generate_account_number()
         )
